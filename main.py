703cae963c28449c2562c65121be42a1a777be77
banabda <banabda@gmail.com>
2020-12-10 18:39:30 +0700

    updated search feature

--- main.py
+++ main.py
@@ -4,37 +4,60 @@ import time
 import datetime
 
 
-def downLink(animeName, animeList):
-    print('\nSearching the anime from the main page, please wait...')
+def requestUrl(ext):
+    animekompi = requests.get(f'http://animekompi.web.id/{ext}').text
+    soup = BeautifulSoup(animekompi, 'lxml')
+    return soup
+
+
+def downLink(animeName, count):
+    print(f'Search in page = {count}')
+    soup = requestUrl(f'page/{count}/')
+
+    animelist = soup.find_all('div', class_='dtl')
     downloadlist = {}
     animeName_ = animeName.split()
-    for anime in animeList:
+
+    for anime in animelist:
+        match = True
+        matches = []
         title = anime.find('a')
+
         for name in animeName_:
+            if name.isdigit():
+                name = ' '+name+' '
             if name.lower() in title.text.lower():
-                link = []
-                episode_url = title.get('href')
-                print('\n\033[1m'+title.text+'\033[0m\n')
-                print('Getting the anime download link, please wait...\n')
-                episode = requests.get(episode_url).text
-                soup_eps = BeautifulSoup(episode, 'lxml')
+                matches.append(True)
+            else:
+                matches.append(False)
 
-                episode_page = soup_eps.find('div', class_="epsc")
-                undordered_list = episode_page.find_all('ul')
+        for item in matches:
+            match = match and item
 
-                for ind, ul in enumerate(undordered_list):
-                    if ind > 0:
-                        link_list = ul.find('li')
-                        download_link_list = link_list.find_all('a')
+        if match:
+            link = []
+            episode_url = title.get('href')
+            print('\n\033[1m'+title.text+'\033[0m\n')
+            print('Getting the anime download link, please wait...\n')
+            episode = requests.get(episode_url).text
+            soup_eps = BeautifulSoup(episode, 'lxml')
 
-                        for dl in download_link_list:
-                            linkdetail = {}
-                            linkdetail[dl.text] = dl.get('href')
-                            link.append(linkdetail)
+            episode_page = soup_eps.find('div', class_="epsc")
+            undordered_list = episode_page.find_all('ul')
 
-                downloadlist[animeName] = link
-                return downloadlist, title.text
-    return 0
+            for ind, ul in enumerate(undordered_list):
+                if ind > 0:
+                    link_list = ul.find('li')
+                    download_link_list = link_list.find_all('a')
+
+                    for dl in download_link_list:
+                        linkdetail = {}
+                        linkdetail[dl.text] = dl.get('href')
+                        link.append(linkdetail)
+
+            downloadlist[animeName] = link
+            return downloadlist, title.text
+    return 0, 0
 
 
 def todayList(today, animeList):
@@ -50,30 +73,57 @@ def todayList(today, animeList):
     return animelist_
 
 
+def firstChoice(cdf):
+    soup = requestUrl('')
+    animelist = soup.find_all('div', class_='dtl')
+    animes = todayList(cdf, animelist)
+    print("\t======\n"+"\n".join(animes)+"\n\t======")
+
+
+def secondChoice():
+    global count
+    anime_name = input('Give an anime name: ')
+    print('\n\033[1m'+"Searching the anime..."+'\033[0m\n')
+    soup = requestUrl('')
+    pagerawtext = soup.find('span', class_="pages").text
+    pagetext = pagerawtext.split()
+    page = int(pagetext[-1].replace(",", ""))
+
+    start = time.time()
+
+    count = 1
+
+    while count <= page:
+        result, title = downLink(anime_name, count)
+        if result != 0:
+            break
+        count += 1
+
+    end = time.time()
+
+    if result == 0:
+        print('\n\033[1m'+"Anime not found"+'\033[0m\n')
+    else:
+        for index, link in enumerate(result[anime_name]):
+            for l in link:
+                server = l
+            for x in link:
+                url = link[x]
+            with open(f'C:/Users/Banabda/Documents/Python/Anime scraping/Result/{title}.txt', 'a') as f:
+                f.write(f'{index+1}. {server} : {url}\n')
+        print('\033[1m'+f"Link for \"{title}\" Saved!"+'\033[0m\n')
+    print('Time taken in seconds -', end - start)
+
+
+# Run
 print('Welcome to anime scraping!')
-animekompi = requests.get('http://animekompi.web.id/').text
-soup = BeautifulSoup(animekompi, 'lxml')
-animelist = soup.find_all('div', class_='dtl')
 while True:
     inChoice = input('1. Today anime list; 2. Download link; 3. Close : ')
     cdf = datetime.datetime.today().strftime('%A')
     if inChoice == "1":
-        animes = todayList(cdf, animelist)
-        print("\t======\n"+"\n".join(animes)+"\n\t======")
+        firstChoice(cdf)
     elif inChoice == "2":
-        anime_name = input('Give an anime name: ')
-        result, title = downLink(anime_name, animelist)
-        if result == 0:
-            print('\n\033[1m'+"Anime not found"+'\033[0m\n')
-        else:
-            for index, link in enumerate(result[anime_name]):
-                for l in link:
-                    server = l
-                for x in link:
-                    url = link[x]
-                print(f"{server} : {url}")
-                with open(f'C:/Users/Banabda/Documents/Python/Anime scraping/Result/{title}.txt', 'a') as f:
-                    f.write(f'{index+1}. {server} : {url}\n')
+        secondChoice()
     elif inChoice == "3":
         print("Thank you! See you later! :)")
         break
